c1ccac2d3ca5e7d15fc02fbfab05708971e7535d
sidney bernardin <sidney@sidney-debian-vm.spb.net>
2020-11-13 20:27:25 -0500

    Started on the Admin function.

--- gcloud_funcs.go
+++ gcloud_funcs.go
@@ -10,7 +10,6 @@ import (
 	"time"
 
 	"cloud.google.com/go/datastore"
-	"github.com/Sidney-Bernardin/bungo"
 )
 
 // Entry defines a log entry.
@@ -38,12 +37,14 @@ func (e Entry) String() string {
 	return string(out)
 }
 
-// CreateUser is a Google Cloud Function for creating users.
-// After it asks the user for their destiny info, it adds them
-// to a Google Cloud Datastore, Database.
-func CreateUser(w http.ResponseWriter, r *http.Request) {
+// Admin responds with a template allowing an administrator to login
+// and manage users.
+func Admin(w http.ResponseWriter, r *http.Request) {
 
-	var trace string
+	var (
+		trace string
+		users = []modelUser{}
+	)
 
 	// Derive the traceID associated with the current request.
 	traceHeader := r.Header.Get("X-Cloud-Trace-Context")
@@ -52,100 +53,31 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 		trace = fmt.Sprintf("projects/%s/traces/%s", projectID, traceParts[0])
 	}
 
-	// If form was submited (if request method is POST), create and store the
-	// user into the database.
-	if r.Method == "POST" {
+	switch r.URL.Query()["process"][0] {
 
-		// Get form values.
-		username := r.FormValue("username")
-		membershipType := r.FormValue("membershipType")
-		gamertag := r.FormValue("gamertag")
+	case "link_with_bungie":
 
-		// Create bungo service.
-		s, err := bungo.NewService(&http.Client{}, apiKey)
-		if err != nil {
-
-			log.Println(Entry{
-				Message:  err.Error(),
-				Severity: "CRITICAL",
-				Trace:    trace,
-			})
-
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+		// Authenticate the login.
+		password := r.FormValue("password")
+		if password != adminPassword {
+			http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 			return
 		}
 
-		// Use SearchDestinyPlayer to get the membershipID.
-		res, err := s.Destiny2.SearchDestinyPlayer(membershipType, gamertag).Do()
-		if err != nil {
+	case "home":
 
-			log.Println(Entry{
-				Message:  err.Error(),
-				Severity: "CRITICAL",
-				Trace:    trace,
-			})
-
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+		// Authenticate the login.
+		password := r.FormValue("password")
+		if password != adminPassword {
+			http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 			return
-		}
-
-		// Check if the destiny player was found.
-		if len(res.Response) == 0 {
-
-			err := "couldn't find destiny player: " + gamertag
-			http.Error(w, err, http.StatusNotFound)
-			return
-		}
-
-		// Extract the membershipID from the response.
-		membershipID := res.Response[0].MembershipID
-
-		// Use Profile to get the characterIDs.
-		res2, err := s.Destiny2.GetProfile(membershipType, membershipID).
-			Components("Profiles").Do()
-		if err != nil {
-
-			log.Println(Entry{
-				Message:  err.Error(),
-				Severity: "CRITICAL",
-				Trace:    trace,
-			})
-
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		// Extract the characterIDs from the response.
-		characterIDs := res2.Profiles.Response.Profile.Data.CharacterIds
-		switch len(characterIDs) {
-		case 0:
-			characterIDs = []string{"", "", ""}
-		case 1:
-			characterIDs = []string{characterIDs[0], "", ""}
-		case 2:
-			characterIDs = []string{characterIDs[0], characterIDs[1], ""}
-		case 3:
-			characterIDs = []string{characterIDs[0], characterIDs[1], characterIDs[2]}
-		}
-
-		// Assemble the user.
-		user := modelUser{
-			Username:       username,
-			Gamertag:       gamertag,
-			MembershipType: membershipType,
-			MembershipID:   membershipID,
-			Characters: []modelCharacter{
-				{ID: characterIDs[0]},
-				{ID: characterIDs[1]},
-				{ID: characterIDs[2]},
-			},
 		}
 
 		// Create context for datastore.
 		ctx, cancel := context.WithTimeout(context.Background(), 9*time.Second)
 		defer cancel()
 
-		// Create a datastore client.
+		// Create datastore client.
 		c, err := datastore.NewClient(ctx, projectID)
 		if err != nil {
 
@@ -159,9 +91,9 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		// Store the user into the database.
-		key := datastore.IncompleteKey("User", nil)
-		if _, err := c.Put(ctx, key, &user); err != nil {
+		// Get all user from the database.
+		q := datastore.NewQuery("User")
+		if _, err := c.GetAll(ctx, q, &users); err != nil {
 
 			log.Println(Entry{
 				Message:  err.Error(),
@@ -172,17 +104,23 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-
-		return
 	}
 
-	// Execute the, create_user.html template.
-	err := temps.ExecuteTemplate(w, "create_user.html", map[string]string{
-		"CreateUserEndpoint": createUserEndpoint,
+	// Execute template and pass the users into it.
+	err := temps.ExecuteTemplate(w, "admin.html", map[string]interface{}{
+		"Users": users,
 	})
 
 	if err != nil {
+
+		log.Println(Entry{
+			Message:  err.Error(),
+			Severity: "CRITICAL",
+			Trace:    trace,
+		})
+
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 }
 
--- vars.go
+++ vars.go
@@ -6,8 +6,8 @@ import (
 )
 
 var (
-	temps              *template.Template
-	apiKey             = os.Getenv("BUNGIE_API_KEY")
-	projectID          = os.Getenv("PROJECT_ID")
-	createUserEndpoint = os.Getenv("CREATE_USER_ENDPOINT")
+	temps         *template.Template
+	apiKey        = os.Getenv("BUNGIE_API_KEY")
+	projectID     = os.Getenv("PROJECT_ID")
+	adminPassword = os.Getenv("ADMIN_PASSWORD")
 )
